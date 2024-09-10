edf3bee6bb0d30dd68d975ceeb78a391d035a38d
Nika <kuromatka@gmail.com>
2024-09-10 22:04:45 +0300

    add post size function

---
+++ public/assets/post_size.js
@@ -0,0 +1,21 @@
+export default function postSize(message) {
+  const start = ['http://', 'https://', 'www.'];
+  const end = ['.com', '.org', '.net', '.ru', '.io', '.gov', '.edu', '.uk', '.de', '.jp'];
+  const arr = [];
+  const words = message.split(' ');
+  words.forEach(function(word) {
+    const url = start.some(function(starting) {
+      return word.includes(starting);
+    });
+    const domain = end.some(function(ending) {
+      return word.includes(ending);
+    });
+    if (url || domain) {
+      arr.push(word);
+    }
+  });
+  const filter = words.filter(function(word) {
+    return !arr.includes(word);
+  });
+  return filter.join(' ').length;
+}
