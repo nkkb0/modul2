701b7c175d1dd91b1dc7875111cdd9a7cf656e89
Nika <kuromatka@gmail.com>
2024-10-25 11:33:21 +0300

    одна конвертация в number

--- public/assets/convert_time.js
+++ public/assets/convert_time.js
@@ -4,52 +4,52 @@ export default function convertTime(posted, now) {
   const hours = Math.floor(seconds / 3600);
   const days = Math.floor(seconds / 86400);
   const years = Math.floor(seconds / 31536000);
-  const mEnd = String(minutes).at(-1);
-  const mEnd2 = String(minutes).at(-2);
-  const hEnd = String(hours).at(-1);
-  const dEnd = String(days).at(-1);
-  const yEnd = String(years).at(-1);
+  const mEnd = Number(String(minutes).at(-1));
+  const mEnd2 = Number(String(minutes).at(-2));
+  const hEnd = Number(String(hours).at(-1));
+  const dEnd = Number(String(days).at(-1));
+  const yEnd = Number(String(years).at(-1));
   if (years > 0) {
-    if (yEnd === '1') {
+    if (yEnd === 1) {
       return `${years} год назад`;
     }
-    if (yEnd > '1' && yEnd < '5') {
+    if (yEnd > 1 && yEnd < 5) {
       return `${years} года назад`;
     }
-    if (yEnd >= '5' || yEnd === '0') {
+    if (yEnd >= 5 || yEnd === 0) {
       return `${years} лет назад`;
     }
   }
   if (days > 0) {
-    if (dEnd === '1') {
+    if (dEnd === 1) {
       return `${days} день назад`;
     }
-    if (dEnd > '1' && dEnd < '5') {
+    if (dEnd > 1 && dEnd < 5) {
       return `${days} дня назад`;
     }
-    if (dEnd >= '5' || yEnd === '0') {
+    if (dEnd >= 5 || yEnd === 0) {
       return `${days} дней назад`;
     }
   }
   if (hours > 0) {
-    if (hEnd === '1') {
+    if (hEnd === 1) {
       return `${hours} час назад`;
     }
-    if (hEnd > '1' && yEnd < '5') {
+    if (hEnd > 1 && yEnd < 5) {
       return `${hours} часа назад`;
     }
-    if (hEnd >= '5' || yEnd === '0') {
+    if (hEnd >= 5 || yEnd === 0) {
       return `${hours} часов назад`;
     }
   }
   if (minutes > 0) {
-    if (mEnd === '1' && mEnd2 !== '1') {
+    if (mEnd === 1 && mEnd2 !== 1) {
       return `${minutes} минуту назад`;
     }
-    if (mEnd > '1' && mEnd < '5' && mEnd2 !== '1') {
+    if (mEnd > 1 && mEnd < 5 && mEnd2 !== 1) {
       return `${minutes} минуты назад`;
     }
-    if (mEnd >= '5' || yEnd === '0' || (mEnd2 === '1')) {
+    if (mEnd >= 5 || yEnd === 0 || (mEnd2 === 1)) {
       return `${minutes} минут назад`;
     }
   }
