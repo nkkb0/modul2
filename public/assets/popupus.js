2fb8cfd86215997bc38f820e298c9672ddd7bbeb
Nika <kuromatka@gmail.com>
2025-01-29 17:01:19 +0300

    linter changes

--- public/assets/popupus.js
+++ public/assets/popupus.js
@@ -1,20 +1,20 @@
-
+/* eslint-env browser */
 const popuplinks = document.querySelectorAll('.popup-link_reg');
 const popUpreg = document.getElementById('popup1');
-let touchstartY, touchendY;
-
+let touchstartY;
+let touchendY;
 for (let i = 0; i < popuplinks.length; i += 1) {
   const popuplink = popuplinks[i];
-  popuplink.addEventListener("click", () => {
+  popuplink.addEventListener('click', () => {
     popUpreg.classList.add('open');
   });
-};
+}
 
-document.addEventListener('touchstart', e => {
+document.addEventListener('touchstart', (e) => {
   touchstartY = e.changedTouches[0].screenY;
 });
 
-document.addEventListener('touchend', e => {
+document.addEventListener('touchend', (e) => {
   touchendY = e.changedTouches[0].screenY;
   if (touchendY > touchstartY) {
     popUpreg.classList.remove('open');
@@ -26,16 +26,16 @@ const popUpent = document.getElementById('popup2');
 
 for (let i = 0; i < popuplinksEnt.length; i += 1) {
   const popuplink = popuplinksEnt[i];
-  popuplink.addEventListener("click", () => {
+  popuplink.addEventListener('click', () => {
     popUpent.classList.add('open');
   });
-};
+}
 
-document.addEventListener('touchstart', e => {
+document.addEventListener('touchstart', (e) => {
   touchstartY = e.changedTouches[0].screenY;
 });
 
-document.addEventListener('touchend', e => {
+document.addEventListener('touchend', (e) => {
   touchendY = e.changedTouches[0].screenY;
   if (touchendY > touchstartY) {
     popUpent.classList.remove('open');
